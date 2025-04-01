171d363410795d05ab7dffc5a4cd72518679b3b1
Alma29 <alma.uni16@gmail.com>
2025-04-01 10:45:58 -0600

    Se hicieron cambios esteticos en index y se agregaron imgs.

---
+++ src/components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+    <footer class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+      <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        </svg>
+        <span class="ml-3 text-xl">RECOCOIN</span>
+      </a>
+      <p class="mt-2 text-sm text-gray-500">REFRIGERACIÓN COMERCIAL Y COCINAS INDUSTRIALES</p>
+    </div>
+    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">REFRIGERACIÓN COMERCIAL</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Enfriadores</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Congeladores</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Vitrinas</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Cámaras Frías</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">COCINA</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Cocción</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Lava vajillas</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Utensilios/vajillas</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">cuchillería</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">BÁSCULAS Y MAQUINARIA</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Básculas</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Molinos</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Rebanadoras</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Procesadoras</a>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">REFACCIONES Y ACCESORIOS</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Gases refrigerantes</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Compresores</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Controles</a>
+          </li>
+          <li>
+            <a class="text-gray-600 hover:text-gray-800">Accesorios</a>
+          </li>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="bg-gray-100">
+    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <p class="text-gray-500 text-sm text-center sm:text-left">RECOCOIN© — 2025
+       
+      </p>
+      <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+        <a class="text-gray-500">
+          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+          </svg>
+        </a>
+        
+        <a class="ml-3 text-gray-500">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+          </svg>
+        </a>
+        
+      </span>
+    </div>
+  </div>
+</footer>
+</template>
