1f3c4ac6f6745123d4a7120dc1ce635f7f4c07a7
isaacddr <isaacovic00@gmail.com>
2025-10-14 16:57:32 -0600

    Implement code changes to enhance functionality and improve performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [vue(), tailwind()],
   output: 'static',
-  base: '/Recocoin', // Replace with your repository name
-  site: 'https://isaacddr.github.io', // Replace with your GitHub username
+  site: 'www.recocoin.com.mx', // Replace with your GitHub username
   trailingSlash: 'always',
   build: {
     format: 'directory'
